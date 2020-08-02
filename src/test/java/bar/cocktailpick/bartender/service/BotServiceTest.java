a3cb7fdaed09b84cccf9f1e593cb825d28ad79a3
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-08-03 02:25:12 +0900

    hotfix: 테스트 깨짐 수정

--- src/test/java/bar/cocktailpick/bartender/service/BotServiceTest.java
+++ src/test/java/bar/cocktailpick/bartender/service/BotServiceTest.java
@@ -56,9 +56,9 @@ class BotServiceTest {
         when(request.is(any())).thenReturn(false);
         when(request.is(Command.REVIEW)).thenReturn(true);
         when(request.getUser_name()).thenReturn("그니");
-        when(request.getChannel_id()).thenReturn("C2147483705");
+        when(request.getChannel_name()).thenReturn("제발");
 
-        assertThat(botService.serve(request).getText()).contains("<@C2147483705>", "리뷰", "그니");
+        assertThat(botService.serve(request).getText()).contains("<@제발>", "리뷰", "그니");
     }
 
     @Test
