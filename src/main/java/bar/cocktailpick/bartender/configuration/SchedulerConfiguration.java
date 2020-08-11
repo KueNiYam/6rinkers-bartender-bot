ab73eb4618031eb34634c2315e5eabcf327ed0e1
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-08-11 19:57:39 +0900

    feat: Scheduler Thread Pool 설정

---
+++ src/main/java/bar/cocktailpick/bartender/configuration/SchedulerConfiguration.java
@@ -0,0 +1,22 @@
+package bar.cocktailpick.bartender.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+public class SchedulerConfiguration implements SchedulingConfigurer {
+    private static final int POOL_SIZE = 10;
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+        threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("6rinkers-scheduled-task-pool-");
+        threadPoolTaskScheduler.initialize();
+
+        scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+}
