6e1d90531cacdb3236d58c6becb9708ca33b49fb
Kuenhwi <34573901+KueNiYam@users.noreply.github.com>
2020-09-22 23:33:15 +0900

    fix: RoleMemberQueryRepositoryTest의 profile을 local로 설정

--- src/test/java/bar/cocktailpick/bartender/domain/rolemembers/RoleMemberQueryRepositoryTest.java
+++ src/test/java/bar/cocktailpick/bartender/domain/rolemembers/RoleMemberQueryRepositoryTest.java
@@ -4,6 +4,7 @@ import bar.cocktailpick.bartender.domain.rolemembers.dto.RoleMemberCountDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles(profiles = "local")
 class RoleMemberQueryRepositoryTest {
 
     @Autowired
